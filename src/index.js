7c19c6010e29e5cfaf599dc0689e22f74cdc8179
woody <5385kim@gmail.com>
2020-11-29 13:10:04 +0900

    #1.4 Counter finish

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-//@ts-check
 import { createStore } from "redux";
 
 const up = document.getElementById("up");
 const down = document.getElementById("down");
 const number = document.querySelector("span");
 
+const Counter = {
+  ADD: 1,
+  DOWN: 2,
+};
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
   switch (action.type) {
-    case "up":
-      count++;
-      break;
-    case "down":
-      count--;
-      break;
+    case Counter.ADD:
+      return count + 1;
+    case Counter.DOWN:
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 };
 const countStore = createStore(countModifier);
 countStore.subscribe(() => {
   number.innerText = countStore.getState();
 });
 
-up.addEventListener("click", () => countStore.dispatch({ type: "up" }));
-down.addEventListener("click", () => countStore.dispatch({ type: "down" }));
+up.addEventListener("click", () => countStore.dispatch({ type: Counter.ADD }));
+down.addEventListener("click", () => countStore.dispatch({ type: Counter.ADD }));
